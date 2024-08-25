f113d7737557e279e376484ee5cffb28904c1841
toptobes <kavinpg@gmail.com>
2024-08-25 18:24:14 -0500

    cleaned up tf module inputs/outputs

--- templates/interpreter/end.tf
+++ templates/interpreter/end.tf
@@ -6,10 +6,6 @@ output "output" {
   value = local.iter_\{prev_index}.output
 }
 
-output "code_ptr" {
-  value = local.iter_\{prev_index}.code_ptr
-}
-
-output "steps_taken" {
-  value = local.iter_\{prev_index}.steps_taken
+output "final_iteration" {
+  value = local.iter_\{prev_index}
 }
